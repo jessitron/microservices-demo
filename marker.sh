cf50ce2892b3ca5292fabdbdfbb236309c74c115
Jessica Kerr <jessitron@honeycomb.io>
2022-07-13 10:27:09 -0500

    Support timestamp in marker.sh

--- marker.sh
+++ marker.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+set -e -x 
+
 DATASET=checkout
+
+if [[ -n "$TIMESTAMP" ]]
+then
+  timestamp_portion=", \"start_time\": $TIMESTAMP "
+fi
 
 if [[ -z "$HONEYCOMB_APIKEY" ]]
 then
@@ -10,4 +17,4 @@ fi
 
 curl https://api.honeycomb.io/1/markers/$DATASET -X POST \
   -H "X-Honeycomb-Team: $HONEYCOMB_APIKEY" \
-  -d '{"message": "deploy build #45362", "type": "deploy"}'
+  -d "{\"message\": \"deploy build #45362\", \"type\": \"deploy\" $timestamp_portion}"
